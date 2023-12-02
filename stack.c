6b63a591788dc3a41c04ff7987ae310836bcdee8
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2023-12-02 19:27:41 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+void push();
+void pop();
+void display();
+int arr[5], top=0;
+void main(){   
+    int ch;
+    do{
+        printf("\n\n---------Stack Operation---------\n\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter Your Choice: ");
+        scanf("%d",&ch);
+        switch(ch) {
+            case 1: push();
+                break;
+            case 2: pop();
+                break;
+            case 3: display();
+                break;
+            case 4: printf("Thank You for using RDS Code");
+                break;
+            default: printf("Invalid Choice"); 
+                return;       
+        }
+    }while(ch!=4);
+}
+
+void push(){
+    int num;
+    if(top==5){
+        printf("Stack is Full");
+        return;
+    }
+    else{
+        printf("Enter a number: ");
+        scanf("%d",&num);
+        arr[top]=num;
+        top++;
+        return;
+    }
+}
+
+void pop(){
+    int num;
+    if(top==0){
+        printf("Stack is Empty");
+        return;
+    }
+    else{
+        top--;
+        return;
+    }
+}
+
+void display(){
+    if(top==0){
+        printf("Stack is Empty");
+        return;
+    }
+    else{
+       printf("Stack is: ");
+    for(int i=0;i<top;i++){
+        printf("%d ",arr[i]);
+    }
+    return;
+    }
+    
+}
